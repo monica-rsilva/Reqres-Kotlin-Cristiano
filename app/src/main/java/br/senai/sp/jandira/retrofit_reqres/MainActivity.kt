8f5048cb1a988e86c095b04a2c34994b36adf581
MÔNICA RIBEIRO DA SILVA <22282167@dsn127m1241955.cfp127aluno.local>
2023-08-18 10:33:50 -0300

    put,post e delete inseridos

--- app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.lifecycle.lifecycleScope
+import com.google.gson.JsonObject
 import kotlinx.coroutines.launch
 import retrofit2.create
 
@@ -17,18 +18,34 @@ class MainActivity : AppCompatActivity() {
         apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
 
 //        Açao do botão GET:
-        findViewById<Button>(R.id.btnGET).setOnClickListener{
+        findViewById<Button>(R.id.btnGET).setOnClickListener {
             getUserByID()
+        }
+
+        //        Açao do botão POST:
+        findViewById<Button>(R.id.btnPOST).setOnClickListener {
+            createUser()
+        }
+
+        //        Açao do botão PUT:
+        findViewById<Button>(R.id.btnPUT).setOnClickListener {
+            updateUser()
+        }
+
+        //        Açao do botão PUT:
+        findViewById<Button>(R.id.btnDELETE).setOnClickListener {
+            deleteUser()
         }
     }
 
+    //    Recupera dados de usuario
     private fun getUserByID() {
 
         lifecycleScope.launch {
 //            chamada para endpoint
             val result = apiService.getUserByID("2")
 
-            if(result.isSuccessful){
+            if (result.isSuccessful) {
 
                 Log.e("GETTING-DATA", "${result.body()?.data}")
 
@@ -39,4 +56,70 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    //    Insere dados de usuario
+    private fun createUser() {
+        lifecycleScope.launch {
+            val body = JsonObject().apply {
+                addProperty("Name", "Monica")
+                addProperty("job", "Developer")
+                addProperty("", "")
+                addProperty("", "")
+            }
+
+            val result = apiService.createUser(body)
+
+            if (result.isSuccessful) {
+
+                Log.e("CREATE-DATA", "${result.body()}")
+
+            } else {
+
+                Log.e("CREATE-DATA", "${result.message()}")
+
+            }
+        }
+    }
+
+    //    Altera dados de usuario
+    private fun updateUser() {
+        lifecycleScope.launch {
+            val body = JsonObject().apply {
+                addProperty("Name", "Monica R. S.")
+                addProperty("job", "Developer")
+            }
+
+            val result = apiService.updateUser("2", body)
+
+            if (result.isSuccessful) {
+
+                Log.e("UPDATE-DATA", "${result.body()}")
+
+            } else {
+
+                Log.e("UPDATE-DATA", "${result.message()}")
+
+            }
+        }
+    }
+
+    //    Altera dados de usuario
+    private fun deleteUser() {
+        lifecycleScope.launch {
+
+            val result = apiService.deleteUser("2")
+
+            if (result.isSuccessful) {
+
+                Log.e("DELETE-DATA", "${result}")
+
+            } else {
+
+                Log.e("DELETE-DATA", "${result.message()}")
+
+            }
+        }
+    }
+
+
 }
